bd342773143d040bc087039b01938702aba592de
martindevaprasad <martindevaprasad46@gmail.com>
2023-03-01 19:55:01 +0530

    support

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
-import './style.css';
-import './support.css';
-import Header from './Header';
-import Givesupport from './Givesupport';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Givesupport } from "./components/Givesupport";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Givesupport />
-      </div>
-    );
-  }
-}
+import Header from "./components/Header";
+import { Register } from "./components/Register";
+import "./style.css";
+import "./support.css";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/*" element={<Givesupport />} />
+
+        <Route path="/login" element={<Register />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
